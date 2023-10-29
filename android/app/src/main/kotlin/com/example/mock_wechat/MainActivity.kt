175b3ec3b2f71fd8fd5d5b0bb3400bfc324d238c
mingyue-jiang <jay.l.jiang@gmail.com>
2023-10-29 18:11:07 +0800

    引入了picker插件

---
+++ android/app/src/main/kotlin/com/example/mock_wechat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mock_wechat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'pages/post.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '模拟微信',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const PostEditPage(),
+    );
+  }
+}
